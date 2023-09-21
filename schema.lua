9eea7980491aa0c2ec8fe91d9151c97f5b61924c
Longkunz <cuvip1325@gmail.com>
2023-09-21 10:51:17 +0700

    update

--- schema.lua
+++ schema.lua
@@ -2,23 +2,17 @@ local typedefs = require "kong.db.schema.typedefs"
 
 return {
     name = "oidc-acl",
-    fields = { {
-        consumer = typedefs.no_consumer
-    }, {
-        protocols = typedefs.protocols_http
-    }, {
-        config = {
-            type = "record",
-            fields = {
-                { allow = { type = "array", elements = { type = "string" }, }, },
-                { userinfo_header_name = { type = "string", required = false, default = "x-userinfo" }, },
-            },
-            entity_checks = { {
-                only_one_of = { "config.allow", "config.deny" }
-            }, {
-                at_least_one_of = { "config.allow", "config.deny" }
-            } }
-
+    fields = {
+        { consumer = typedefs.no_consumer },
+        { protocols = typedefs.protocols_http },
+        {
+            config = {
+                type = "record",
+                fields = {
+                    { allow = { type = "array", elements = { type = "string" }, }, },
+                    { userinfo_header_name = { type = "string", required = false, default = "x-userinfo" }, },
+                }
+            }
         }
-    } }
+    }
 }
